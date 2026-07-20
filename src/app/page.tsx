eba9fee0ac6d26b1e2cebea220eef1818cf0feb1
RaeannW <raeannw321@gmail.com>
2026-07-20 13:38:49 -0500

    created layout for ProjectSection comoponent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Header from "./components/sections/Header/Header";
 import Hero from "./components/sections/Hero/Hero";
 import PageSheet from "./components/sections/PageSheet/PageSheet";
+import ProjectSection from "./components/sections/ProjectSection/ProjectSection";
 
 export default function Home() {
   return (
     <PageSheet>
       <Header />
       <Hero />
+      <ProjectSection />
     </PageSheet>
 
   );
